9f73baba57541008b040d6bac566d71323346e7d
Alejohax <alexander.johanssonn@gmail.com>
2023-02-21 18:04:35 +0100

    Initial setup of server

---
+++ server/models/cart.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("cart", {
+    cart_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    amount: {
+      type: DataTypes.DOUBLE,
+    },
+  });
+};
