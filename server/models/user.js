c87ef6f7b96f7c0a006e9b9f22dc92308e5e6336
Alejohax <alexander.johanssonn@gmail.com>
2023-02-21 18:07:31 +0100

    Revert "Revert "Initial setup of server""
    This reverts commit e32c7929bc637b659eea4b356ccecac5f0f923ae.

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("user", {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    last_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [8, 100],
+      },
+    },
+  });
+};
